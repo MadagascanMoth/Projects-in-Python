ed9dba40833d5b608efcad13ddf3517bae339c9d
MadagascanMoth <madagascanmoth@gmail.com>
2022-11-19 21:50:07 +0100

    piton

---
+++ MadLibsGame.py
@@ -0,0 +1,8 @@
+
+color = input("Enter your favorite color: ")
+plural_noun = input("Enter a plural noun: ")
+celebrity = input("Enter your favorite celebrity: ")
+
+print("Roses are " + color + ",")
+print(plural_noun + " are blue,")
+print("I love " + celebrity)
---
+++ PythonApp.py
@@ -0,0 +1,22 @@
+print("Hello World")
+
+print("   /|")
+print("  / |")
+print(" /  |")
+print("/___|")
+
+character_name = "Tom"
+character_age = "55"
+
+print("There was a name called " + character_name + ", ")
+print("he was " + character_age + " years old.")
+print(character_name + " liked his name a lot, ")
+print("but he didn't like being " + character_age + " years old.")
+
+phrase = "Ekipa najjaca"
+print(phrase.lower())
+print(phrase.replace("Ekipa", "Grupa"))
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+print("Hello " + name + "! Nice to meet you. You are " + age + ", very young")
